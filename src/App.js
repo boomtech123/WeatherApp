d95d47b4fed5ba1394f1b026bab0a99cf9b5a3c3
Rajsolkar12345 <109979277+Rajsolkar12345@users.noreply.github.com>
2023-10-11 14:40:58 +0530

    Update App.js
    Package Time and Location is Imported successfully

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
