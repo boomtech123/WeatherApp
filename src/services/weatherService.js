a80436775f6fa88587109b5fde4b8c969a7c52ef
Rajsolkar12345 <109979277+Rajsolkar12345@users.noreply.github.com>
2023-10-11 14:53:41 +0530

    Update weatherService.js
    API key is added successfully from openweatherapp

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,7 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "";
+
+const API_KEY = "838525df7399df50e130d8d402966a1f";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
